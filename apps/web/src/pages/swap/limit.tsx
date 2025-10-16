6341907d7146fe6de1df6e8dc670195c9ff71fe7
memoyil <2213635+memoyil@users.noreply.github.com>
2025-10-16 07:36:42 +0200

    fix: Padding

--- apps/web/src/pages/swap/limit.tsx
+++ apps/web/src/pages/swap/limit.tsx
@@ -7,7 +7,7 @@ import { ChainId } from '@pancakeswap/chains'
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <Page showExternalLink={false} showHelpLink={false} removePadding>
+    <Page showExternalLink={false} showHelpLink={false}>
       {children}
     </Page>
   )
